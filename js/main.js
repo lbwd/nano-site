7c1996b64729c22d4463946b8049976bfa855d6b
Luca Benedetti <luca.benedetti@euei.it>
2019-07-31 11:15:30 +0200

    Animation from FLIP to "will-change" for firefox.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,5 @@
-document.getElementsByClassName('top-cover')[0].addEventListener('click', clickTransistion);
-document.getElementsByClassName('cover-image')[0].addEventListener('click', clickTransistion);
-
-function clickTransistion(event) {
-  let elem = document.getElementsByClassName('top-cover')[0];
-  let first = elem.getBoundingClientRect();
-
-  elem.classList.add('moving');
-  elem.classList.add('moved-top');
-  let last = elem.getBoundingClientRect();
-
-  let changeTop = first.top - last.top;
-  elem.style.transform = 'translateY(' + changeTop + 'px)';
-
-  requestAnimationFrame(function() {
-    elem.classList.add('transition');
-    elem.style.transform = '';
-    elem.addEventListener('transitionend', function handler(event) {
-      elem.classList.remove('transition');
-      elem.classList.remove('moving');
-
-      elem.removeEventListener('transitionend', handler);
-    });
+document
+  .getElementsByClassName('top-cover')[0]
+  .addEventListener('click', function(event) {
+    document.getElementsByClassName('top-cover')[0].classList.add('animation');
   });
-}
